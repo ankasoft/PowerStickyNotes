f24aab57e2bd2c5a262e421891ef6a9efdd87116
Vehbi Emiroglu <vehbi.emiroglu@gmail.com>
2026-03-23 11:02:23 +0300

    folder changes

--- app/src/main/java/com/example/stickynotes/Note.kt
+++
@@ -1,7 +0,0 @@
-package com.example.stickynotes
-
-data class Note(
-    val id: String = System.currentTimeMillis().toString(),
-    val content: String = "",
-    val createdAt: Long = System.currentTimeMillis()
-)
